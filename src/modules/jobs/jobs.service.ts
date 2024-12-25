45af382512168e5720bfb547ee680c1e1431f3e6
NafiulHasan41 <nhashik40@gmail.com>
2024-12-25 22:53:03 +0600

    Created jobs service expect gell all jobs filtering

---
+++ src/modules/jobs/jobs.service.ts
@@ -0,0 +1,31 @@
+import { IJob } from "./jobs.interface";
+import { JobModel } from "./jobs.model";
+
+const createJob = async (jobData: IJob) => {
+  const job = await JobModel.create(jobData);
+  return job;
+};
+
+const getJobById = async (jobId: string) => {
+  const job = await JobModel.findById(jobId);
+  return job;
+};
+
+const updateJobById = async (jobId: string, updateData: Partial<IJob>) => {
+  const updatedJob = await JobModel.findByIdAndUpdate(jobId, updateData, { new: true });
+  return updatedJob;
+};
+
+const deleteJobById = async (jobId: string) => {
+  const result = await JobModel.findByIdAndDelete(jobId);
+  return result;
+};
+
+
+
+export const jobService = {
+  createJob,
+  getJobById,
+  updateJobById,
+  deleteJobById,
+};
